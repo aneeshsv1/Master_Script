cbf7e91ab5b5b4ae6026fb2d5a54cdd8d898e948
aneeshsv1 <aneeshsv1@gmail.com>
2018-12-12 10:25:52 +0530

    Add files via upload

--- 1.sh
+++ 1.sh
@@ -2,51 +2,51 @@ function userstatus
 {
  sleep 1
  echo -n -e "\e[35mEnter User/User-id Name: \e[0m"
- read selection
- id $selection 1>>/dev/null
+ read user
+ id $user > /dev/null 2>&1
  Result=`echo $?`
  if [[ $Result == 0 ]]
  then
-  sleep 2
-  echo -e "\e[32mUser exists...Checking for Account lock and Blank password\e[0m"
-  sleep 2  
-  Shells1=`cat /etc/passwd | grep -w $selection | cut -d ":" -f 7`
-  Shells2=`cat /etc/shells | grep $Shells1`
-  SystemShells=`echo $?`
-  function CheckShadowLock
-  {
-   if [[ `cat /etc/shadow | awk -F ":" '$1 == "$selection" {print $2}'` == '!!'* ]]
-   then return 99
-   else
-    if [[ `cat /etc/shadow | awk -F ":" '$1 == "$selection" {print $2}'` == '!'* ]]
-    then return 98
-    fi
-   fi
-  }
-  CheckShadowLock
-  PasswdExpiredShadow=`echo $?`
-  BlankPasswd=`awk -F":" '($2 == "!!" || $2 == "*") {print $1}' /etc/shadow | grep $selection`
-  PasswdExpiredPam=`pam_tally2 --user=$selection | awk '{print $2}'|grep -o -E '[0-9]+'`  
-  PasswdExpiredChage=`/usr/bin/chage -l $selection | grep "Password expires"| awk -F ":" '{print $2}'`
+  sleep 1
+  echo -e "\e[32mUser exists...Checking for Account lock and blank password\e[0m"
+  sleep 1  
  else
   echo -e "\e[31mUser doesn't exist\e[0m" && exit 0
-  sleep 2
+  sleep 1
  fi
+ Shells1=`cat /etc/passwd | grep -w $user | cut -d ":" -f 7`
+ Shells2=`cat /etc/shells | grep $Shells1`
+ SystemShells=`echo $?`
+ function CheckShadowLock
+ {
+  if [[ `cat /etc/shadow | grep -w $1 | cut -d ":" -f 2` == '!!'* ]]
+  then return 99
+  else
+   if [[ `cat /etc/shadow | grep -w $1 | cut -d ":" -f 2` == '!'* ]]
+   then return 98
+   fi
+  fi
+ }
+ CheckShadowLock $user
+ PasswdExpiredShadow=`echo $?`
+ BlankPasswd=`awk -F":" '($2 == "!!" || $2 == "*") {print $1}' /etc/shadow | grep $user`
+ PasswdExpiredPam=`pam_tally2 --user=$user | awk '{print $2}'|grep -o -E '[0-9]+'`  
+ PasswdExpiredChage=`/usr/bin/chage -l $user | grep "Password expires"| awk -F ":" '{print $2}'`
  if [[ $SystemShells == 0 ]]
  then
   echo -e "\e[32mUser has a valid shell\e[0m"
-  sleep 2
+  sleep 1
  else
-  echo -e "\e[31mUser not holding valid login shell , current shell is $Shells1\e[0m"
-  sleep 2
+  echo -e "\e[31mUser not holding valid login shell, current shell is $Shells1\e[0m"
+  sleep 1
  fi
- if [[ $BlankPasswd == $selection ]]
+ if [[ $BlankPasswd == $user ]]
  then
   echo -e "\e[31mUser having blank password , check with user before setting the password\e[0m"
-  sleep 2
+  sleep 1
  else
-  echo -e "\e[32mUser has a valid password\e[0m"
-  sleep 2
+  echo -e "\e[32mUser has a non-blank password\e[0m"
+  sleep 1
  fi
  cat /etc/ssh/sshd_config | egrep "^AllowUsers | ^AllowGroups" > /dev/null
  ssh_allow=$?
@@ -54,10 +54,10 @@ function userstatus
  ssh_deny=$?
  if [[ $ssh_allow -eq 0 || $ssh_deny -eq 0 ]]
  then
-  echo -e "\e[31mSshd level restrictions in place for this system\e[0m"
-  sleep 2
+  echo -e "\e[31msshd level restrictions in place for this system\e[0m"
+  sleep 1
   echo "Checking for this particular user"
-  sleep 2
+  sleep 1
   cat /etc/ssh/sshd_config | egrep "^AllowUsers | ^AllowGroups" | grep aneesh1 > /dev/null
   user_ssh_allow=$?
   for i in `groups aneesh1 | awk -F ":" '{print $2}' | xargs -n 1`
@@ -80,56 +80,77 @@ function userstatus
   fi
  else 
   echo -e "\e[32mSshd level restrictions are not in place for this system\e[0m"
- sleep 2
+ sleep 1
  fi 
  if [[ $PasswdExpiredPam == 0 ]]
  then
   echo -e "\e[32mUser Account is not Locked at pam\e[0m"
-  sleep 2
+  sleep 1
  else
   echo -e "\e[35mUser Account is locked at pam and required reset,do you want to continues:\e[0mYes or No\e[0m"
+  sleep 1
   read Option
   if [[ $Option == Yes ]]
+  then
    echo -e "\e[35munlocking the Account\e[0m"
-  pam_tally2 --user=$selection --reset
+  pam_tally2 --user=$user --reset > /dev/null 2>&1
   else
    echo -e "\e[31mPassword Reset request denied by Requester\e[0m"
-   sleep 2
+   sleep 1
   fi
  fi
  if [[ $PasswdExpiredChage == "password must be changed" ]]
  then
-  echo -e "\e[31mUser password expired at chage and required reset,do you want to continues:\e[0mYes or No\e[0m"
+  echo -e "\e[31mUser password expired due to aging and required reset,do you want to continues:\e[0mYes or No\e[0m"
+  sleep 1
   read Option
   if [[ $Option == Yes ]]
   then 
-   echo "Sample123" | passwd --stdin $selection
-   /usr/bin/passwd -e $selection
+   echo "Sample123" | passwd --stdin $user > /dev/null 2>&1
+   /usr/bin/passwd -e $user > /dev/null 2>&1
    echo -e "\e[32mPassword Reset Completed,please share the new password as "Sample123" with requested secured way\e[0m"
-   sleep 2
+   sleep 1
   else
    echo -e "\e[31mPassword Reset request denied by Requester\e[0m"
-   sleep 2
+   sleep 1
   fi
-  if [[ PasswdExpiredShadow -eq 99 ]]
-  then
-   echo -e "\e[35mUser password is locked at shadow and required reset,do you want to continues:\e[0mYes or No\e[0m"
-   read Option
-   if [[ $Option == Yes ]]
-    echo -e "\e[35munlocking the Password\e[0m"
-	passwd -u $selection
-   fi
-  elif [[ PasswdExpiredShadow -eq 98 ]]
-  then
-   echo -e "\e[35mUser Account is locked at shadow and required reset,do you want to continues:\e[0mYes or No\e[0m"
-   read Option
-   if [[ $Option == Yes ]]
-    echo -e "\e[35munlocking the Account\e[0m"
-	usermod -U $selection
-   fi
  else
-   echo -e "\e[32mPassword not expired\e[0m"
-   sleep 2
+  echo -e "\e[32mUser Account is not locked due to aging\e[0m"
+  sleep 1
  fi
-}
+ if [[ PasswdExpiredShadow -eq 99 ]]
+ then
+  echo -n -e "\e[31mUser password is locked at system files and required reset,do you want to continues - \e[0mYes or No : \e[0m"
+  read Option
+  if [[ $Option == [Y,y]es ]]
+  then
+   echo -e "\e[35munlocking the Password\e[0m"
+   sleep 1
+   passwd -u $user > /dev/null 2>&1
+  else
+   echo -e "\e[31mPassword Reset request denied by Requester\e[0m"
+   sleep 1
+  fi
+ else
+ echo -e "\e[32mUser Password is not locked due to system files\e[0m"
+ sleep 1
+ fi
+ if [[ PasswdExpiredShadow -eq 98 ]]
+ then
+  echo -e "\e[35mUser Account is locked at shadow file and required reset,do you want to continues - \e[0mYes or No : \e[0m"
+  read Option
+  if [[ $Option == Yes ]]
+  then
+   echo -e "\e[35munlocking the Account\e[0m"
+   sleep 1
+   usermod -U $user > /dev/null 2>&1
+  else
+   echo -e "\e[31mAccount Reset request denied by Requester\e[0m"
+   sleep 1
+  fi
+ else
+  echo -e "\e[32mUser Account is not locked due to system files\e[0m"
+  sleep 1
+ fi
+} 
 userstatus
